5799b5c05eecfb2b1971fc6a0da454e91e220c3b
Pinfinity <bahramiehsan780@gmail.com>
2023-06-07 10:52:42 +0330

    fix format dicument

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,16 @@ import CardImage from "./CardImage";
 import CardLink from "./CardLink";
 import CardTitle from "./CardTitle";
 import CardParam from "./CardParam";
-import "./Card.css"
+import "./Card.css";
 
 const Card = ({ imageURL, title, param }) => {
   return (
     <div className="card">
       <CardImage imageURL={imageURL} />
-      <div className="card_body" >
-      <CardTitle title={title} />
-      <CardParam param={param} />
-      <CardLink />
+      <div className="card_body">
+        <CardTitle title={title} />
+        <CardParam param={param} />
+        <CardLink />
       </div>
     </div>
   );
